002ac15fb64458e5e182e2392f27b939e05b791b
afrancog1996 <alejandrofrancog95@gmail.com>
2023-05-25 23:18:08 -0600

    feat(main): welcome

---
+++ src/app/material.module.ts
@@ -0,0 +1,19 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+import { NgModule } from '@angular/core';
+
+
+
+@NgModule({
+    imports: [
+        MatToolbarModule
+    ],
+    exports: [
+        MatToolbarModule
+    ],
+    declarations: [],
+    providers: [],
+})
+export class MaterialModule { }
+
